50393b9f5de301277f3dcf9c391371f3e4794708
mcchin <mcchin@gmail.com>
2020-03-30 17:09:36 +0800

    Update Client ID

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 module.exports = {
-    CLIENT_ID: '621480690555-kemineohe830kevrkgvuhhcpali4u2rl.apps.googleusercontent.com',
+    CLIENT_ID: '621480690555-26qujua38f86q46jenh0rn0va7n2ao6i.apps.googleusercontent.com',
     REDIRECT_URI: 'urn:ietf:wg:oauth:2.0:oob',
 };
